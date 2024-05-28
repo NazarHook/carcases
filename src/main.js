77cb2ac2aae0084abec477edfc357001f00ed197
NazarHook <145909670+NazarHook@users.noreply.github.com>
2024-05-28 16:45:27 +0200

    done

--- src/main.js
+++ src/main.js
@@ -1,25 +1,29 @@
-document.getElementById('order_form').addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    const formData = new FormData(event.target);
-    const data = {
-        name: formData.get('name'),
-        phone: formData.get('phone'),
-    };
+import axios from 'axios';
 
-    fetch('/send-email', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    })
-    .then(response => response.json())
-    .then(data => {
-        alert('Email sent successfully!');
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('There was an error sending the email.');
-    });
-});
+const form = document.querySelector('#order_form');
+
+async function handleSubmit(event) {
+    event.preventDefault();
+
+    const name = form.elements.name.value.trim();
+    const phone = form.elements.phone.value.trim();
+
+    if (!name || !phone) {
+        alert('Заповніть всі поля');
+        return;
+    }
+
+    const user = { name, phone };
+    const url = 'https://66557fae3c1d3b602939bfe4.mockapi.io/carcases/carcases';
+
+    try {
+        const response = await axios.post(url, user);
+        window.location.href = 'thankyou.html'
+        form.reset(); 
+        return response.data;
+    } catch (error) {
+        alert('There was an error submitting the data: ' + error.message);
+    }
+}
+
+form.addEventListener('submit', handleSubmit);
